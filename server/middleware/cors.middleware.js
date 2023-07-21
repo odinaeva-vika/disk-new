00ea8f2ef86be1abc4e0134d0dd4a23b20b3f3c7
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-07-22 00:39:48 +0300

    first commit

--- server/middleware/cors.middleware.js
+++ server/middleware/cors.middleware.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// Middleware для CORS
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
@@ -5,4 +9,32 @@ function cors(req, res, next) {
     next();
 }
 
-module.exports = cors
+// Middleware для проверки авторизации через JWT токен
+function checkAuth(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Auth error' });
+        }
+        const decoded = jwt.verify(token, config.get('secretKey'));
+        req.user = decoded;
+        next();
+    } catch (e) {
+        return res.status(401).json({ message: 'Auth error' });
+    }
+}
+
+// Middleware для установки пути к файлам
+function filePath(path) {
+    return function (req, res, next) {
+        req.filePath = path;
+        next();
+    }
+}
+
+// Экспорт массива всех middleware
+module.exports = [cors, checkAuth, filePath];
